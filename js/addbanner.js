0ea7eef7849b0af558494e1bbab50342e5d1b493
lc <1337547531@qq.com>
2020-08-26 00:31:34 +0800

    样式调整

--- js/addbanner.js
+++ js/addbanner.js
@@ -1,4 +1,6 @@
 
+ $(function(){
+    var token = getCookie("token");
     var E = window.wangEditor
     var editor = new E('#wangEditor');
 	//定义上传图片名
@@ -27,11 +29,6 @@
     ]
     editor.create();
  
-	function previewboxClose(){
-        $("#previewbox").hide();
-        $("#previewContent").html("");
-    }
- 
 	$("#viewButton").click(function(){
 		previewboxShow();
 	});
@@ -46,12 +43,26 @@
     
     $("#btn1").click(function(){
         // 获取文件的内容进行上传
-        var token = getCookie("token");
         var obj = document.getElementById("file");
         var files = obj.files;
         var formData = new FormData();
+        var content = editor.txt.html()
+        var title = $("#title").val()
         formData.append("file", files[0])
         console.log(files[0]);
+
+        if (files.length == 0) {
+            layer.msg("请选择一张图片作为封面")
+            return
+        }
+        if (title == '') {
+            layer.msg("标题不能为空")
+            return
+        }
+        if (content == '<p><br></p>') {
+            layer.msg("内容不能为空")
+            return
+        }
         var fileId = null;
         $.ajax({
             //请求方式
@@ -70,7 +81,7 @@
                 var obj = eval('(' + result + ')');
                 if(obj['code'] == 200) {
                     fileId = obj['data']
-                    comfirm(fileId)
+                    comfirm(fileId,title,content)
                 } else if(obj['code'] == 401){
                     console.log("身份信息失效");
                     window.location.href = "login.html";
@@ -87,6 +98,44 @@
     })
 
     // 保存内容
-	function comfirm(id) {
-        alert(id)
+	function comfirm(id, title, content) {
+            var fileId = id
+            var data = {
+                title: title,
+                fileId: fileId,
+                content: content
+            }
+            $.ajax({
+            //请求方式
+            type : "POST",
+            //请求的媒体类型
+            contentType: "application/json;charset=UTF-8",
+            //请求地址
+            url : "http://106.54.213.181:9080/banner/add",
+            //数据，json字符串
+            data : JSON.stringify(data),
+            headers: {
+                    token: token
+            },
+            //请求成功
+            success : function(result) {
+                console.log(result)
+                if(result['code'] == 200) {
+                    console.log(result)
+                    window.location.href = "appcontentmanager.html";
+                } else {
+                    console.log(result['msg'])
+                }
+            },
+            //请求失败，包含具体的错误信息
+            error : function(e){
+                // alert(result)
+            }
+            });
     }
+ })
+
+ function previewboxClose(){
+    $("#previewbox").hide();
+    $("#previewContent").html("");
+}
